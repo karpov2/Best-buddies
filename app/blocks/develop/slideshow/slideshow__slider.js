59b86f1fc5e1026bda8c71f2c050f2a7972fbc44
Igor Kirpinev <ikirpinev@gmail.com>
2019-12-31 17:00:01 +0300

    Добавил слайдер

---
+++ app/blocks/develop/slideshow/slideshow__slider.js
@@ -0,0 +1,33 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".slideshow__slide");
+  let dots = document.querySelectorAll(".slideshow__dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.add("inactive");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].classList.remove("inactive");
+  dots[slideIndex - 1].classList.add("active");
+}
