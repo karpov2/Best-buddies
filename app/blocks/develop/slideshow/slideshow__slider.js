f41246ecced2516b71a564fb962c6b62f50cfb66
Igor Kirpinev <ikirpinev@gmail.com>
2019-12-31 19:22:12 +0300

    Добавил блок coach, переписал js для слайдера

--- app/blocks/develop/slideshow/slideshow__slider.js
+++ app/blocks/develop/slideshow/slideshow__slider.js
@@ -12,21 +12,15 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.querySelectorAll(".slideshow__slide");
-  let dots = document.querySelectorAll(".slideshow__dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].classList.add("inactive");
-  }
+  let slides = document.querySelectorAll(".slideshow__slide"),
+    dots = document.querySelectorAll(".slideshow__dot");
+
+  if (n > slides.length) slideIndex = 1;
+  if (n < 1) slideIndex = slides.length;
+
+  slides.forEach(slide => (slide.style.display = "none"));
+  dots.forEach(dot => dot.classList.add("inactive"));
+
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].classList.remove("inactive");
   dots[slideIndex - 1].classList.add("active");
