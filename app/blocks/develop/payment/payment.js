73fc93cf705d4590797586fccba288d13d9ea40f
Igor Kirpinev <ikirpinev@gmail.com>
2020-01-03 19:19:11 +0300

    Доделал форму, сделал функционал ввода определенной суммы

--- app/blocks/develop/payment/payment.js
+++ app/blocks/develop/payment/payment.js
@@ -1,12 +1,37 @@
-const paymentInputSumAny = document.querySelector('.payment__input-any-sum');
-const paymentSum = document.querySelectorAll('.payment__sum');
-const paymentSumAny = document.querySelectorAll('.payment__sum-any');
+(function() {
+  const paymentForm = document.querySelector(".payment__form");
+  const paymentSum = document.querySelectorAll(".payment__sum");
+  const paymentInput = document.querySelector(".payment__input-any-sum");
+  const rubleSign = document.querySelector(".payment__label-any-sum");
 
-console.dir(paymentInputSumAny);
-console.dir(paymentSum);
+  paymentForm.addEventListener("input", event => {
+    if (
+      event.target.classList.contains("payment__input-any-sum") &&
+      event.target.value.length > 0
+    ) {
+      paymentSum.forEach(sum => {
+        sum.checked = false;
+      });
+      paymentInput.style.border = "none";
+      paymentInput.style.font = "normal bold 18px arial, helvetica";
+      event.target.parentElement.style.border = "2px solid #FC691B";
+      rubleSign.style.color = "#FC691B";
+    } else if (
+      event.target.classList.contains("payment__input-any-sum") &&
+      event.target.value.length === 0
+    ) {
+      paymentInput.style.borderBottom = "1px solid #9397cb";
+      paymentInput.style.font = "normal normal 14px helvetica";
+    }
+  });
 
-const paymentCheckSumAny = () => {
-    console.dir(paymentInputSumAny);
-};
-
-paymentInputSumAny.addEventListener('input', paymentCheckSumAny);
+  paymentForm.addEventListener("focusout", event => {
+    if (
+      event.target.classList.contains("payment__input-any-sum") &&
+      event.target.value.length === 0
+    ) {
+      event.target.parentElement.style.border = "2px solid #9397CB";
+      rubleSign.style.color = "#9397CB";
+    }
+  });
+})();
