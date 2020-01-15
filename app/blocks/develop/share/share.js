dab99fae7fcc4379adc6b210614d0c656b4d87ff
Karpov <karpov.business@gmail.com>
2020-01-15 17:05:59 +0500

    app__wrapper

--- app/blocks/develop/share/share.js
+++ app/blocks/develop/share/share.js
@@ -1,20 +1,14 @@
-const share = document.querySelector('.share');
 const shareImage = document.querySelector('.share__image');
 const smm = document.querySelector('.smm');
 
 const shareSmm = () => {
-    // console.log(event.target.classList.contains('share'));
-    console.dir(event.target);
-    console.dir(shareImage);
-
-    if (event.target.classList.contains('share__image')) {
+    if (event.target.closest('.share__image')) {
         shareImage.classList.add('share__image_disabled');
         smm.classList.remove('smm_disabled');
-    } else {
+    } else if(!event.target.closest('.share')) {
         shareImage.classList.remove('share__image_disabled');
         smm.classList.add('smm_disabled');
     }
 };
 
-share.addEventListener('click', shareSmm);
-// document.addEventListener('click', shareSmm);
+document.addEventListener('click', shareSmm);
